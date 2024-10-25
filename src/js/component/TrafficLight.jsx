ab688efe30d070683589b0e430985c6359731baa
Gabriella Bevilacqua <bevi.gaby@gmail.com>
2024-10-25 20:37:08 +0000

    casi listo

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -4,23 +4,44 @@ const TrafficLight = () => {
 
     // Estado para controlar cuál luz está encendida
     const [selectedLight, setSelectedLight] = useState(null);
+    const [showViolet, setShowViolet] = useState(false)
 
-// el funcionamiento del boton que basicamente es un loop infinito
+    // el funcionamiento del boton que basicamente es un loop infinito
     const toggleLight = () => {
-        if (selectedLight === null || selectedLight === 'green') {
+        if (selectedLight === null) {
             setSelectedLight('red');
         } else if (selectedLight === 'red') {
             setSelectedLight('yellow');
         } else if (selectedLight === 'yellow') {
             setSelectedLight('green');
-        }
+        } else if (selectedLight === 'green' && showViolet) {
+            setSelectedLight('violet');
+        } else { setSelectedLight('red')}
+console.log(selectedLight);
     };
+ /*
+    const toggleLight = () => {
+        if (selectedLight === null) {
+            setSelectedLight('red');
+        } else if (selectedLight === 'red') {
+            setSelectedLight('yellow');
+        } else if (selectedLight === 'yellow') {
+            setSelectedLight('green');
+        } else if (selectedLight === 'green' && showViolet) {
+            setSelectedLight('violet');
+        } else { setSelectedLight('red')}
+console.log(selectedLight);
+    };
+ */
+    const colorExtra = () => {
+        setShowViolet(!showViolet)
+    }
 
     return (
         <>
             <div className="container d-flex justify-content-center align-items-center mt-5">
-                <div className="card text-center">
-                    <div className="card-body d-flex flex-column align-items-center border border-secondary-subtle">
+                <div className="card text-center border border-secondary-subtle">
+                    <div className="card-body d-flex flex-column align-items-center  ">
 
                         <div className={`light red ${selectedLight === 'red' ? 'selected' : ''}`}
                             onClick={() => setSelectedLight('red')}></div>
@@ -30,6 +51,10 @@ const TrafficLight = () => {
 
                         <div className={`light green ${selectedLight === 'green' ? 'selected' : ''}`}
                             onClick={() => setSelectedLight('green')}></div>
+                        {showViolet && (
+                            <div className={`light violet ${selectedLight === 'violet' ? 'selected' : ''}`}
+                                onClick={() => setSelectedLight('violet')}></div>
+                        )}
                     </div>
                 </div>
             </div>
@@ -40,8 +65,11 @@ const TrafficLight = () => {
                 <div className="base border border-secondary" />
             </div>
             <div className="container d-flex justify-content-center align-items-center">
-            <button className="btn btn-secondary mt-3 border border-white" onClick={toggleLight}>
+                <button className="btn btn-secondary mt-3 border border-white" onClick={toggleLight}>
                     Cambiar luz
+                </button>
+                <button className="btn btn-secondary mt-3 ms-2 border border-white" onClick={colorExtra}>
+                    Color extra
                 </button>
             </div>
         </>
